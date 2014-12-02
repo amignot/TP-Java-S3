ca5a20bd9cbfe2566581ebb38b1042f10f22b189
Camille Blaser <camille.blaser@gmail.com>
2014-12-02 19:50:12 +0100

    Mardi 02/12 : exercice 6  Please enter the commit message for your changes. Lines starting

---
+++ src/thread/exo6/notification/TestThreadNotification.java
@@ -0,0 +1,18 @@
+package thread.exo6.notification;
+
+public class TestThreadNotification {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		Compteur compteur = new Compteur();
+		ThreadNotifA pair = new ThreadNotifA(compteur, "pair");
+		ThreadNotifB impair = new ThreadNotifB(compteur, "impair");
+		
+		pair.start();
+		pair.join();
+		impair.start();
+		impair.join();
+
+	}
+
+}
---
+++ src/thread/exo6/notification/ThreadNotifA.java
@@ -0,0 +1,25 @@
+package thread.exo6.notification;
+
+public class ThreadNotifA extends Thread{
+	
+	private final Compteur compteur;
+	
+	public ThreadNotifA(Compteur c, String name) {
+		this.compteur=c;
+		this.setName(name);
+	}
+	
+	@Override
+	public void run() {
+		while (true) {
+			synchronized(this){
+				if(this.compteur.getL() % 2 == 0) {
+					System.out.println(Thread.currentThread().getName()+' '+this.compteur.getL());
+					this.compteur.next();
+				}
+				this.notifyAll();
+				}
+		}
+	}
+
+}
